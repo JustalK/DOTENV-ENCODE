b2e4269331cafe68266fa1b1f596ec6c272e8170
justalk <justal.kevin@gmail.com>
2021-12-30 17:27:26 +0100

    [FEATURE] Adding crypto

---
+++ src/constants/cryptography.ts
@@ -0,0 +1,4 @@
+export const IV_LENGTH: number = 16;
+export const ALGORITHM_AES_256: string = 'aes256';
+export const ALGORITHM_SHA_256: string = 'sha256';
+export const FORMAT_UTF8: 'utf8' = 'utf8';
