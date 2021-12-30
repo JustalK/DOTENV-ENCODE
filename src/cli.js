e87f2ba2e9cea0c2b5e9ea5251e75728407f473c
justalk <justal.kevin@gmail.com>
2021-12-30 06:43:14 +0100

    [FEATURE] Creating the crypto file

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,15 @@
 #! /usr/bin/env node
 
-const argv = require('minimist')(process.argv.slice(2))
-const outputFile = argv.outputFile || argv.o
+const cryptography = require('./cryptography');
+const argv = require('minimist')(process.argv.slice(2));
+
 const inputFile = argv._[0]
+const outputFile = argv.outputFile || argv.o
+const secret = argv.secret || argv.s
+const decrypt = argv.decrypt || argv.d
 
-const { secret } = argv;
-
-console.log(argv, secret)
+if (decrypt) {
+  cryptography.decrypt({ inputFile, outputFile, secret });
+} else {
+  cryptography.encrypt({ inputFile, outputFile, secret });
+}
---
+++ src/cryptography.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const crypto = require('crypto')
+const fs = require('fs')
+
+module.exports = {
+  encrypt: () => {
+    console.log('encrypt');
+  },
+  decrypt: () => {
+    console.log('decrypt');
+  }
+}
